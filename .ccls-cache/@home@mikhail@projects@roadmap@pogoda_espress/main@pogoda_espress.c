10dd315691746612ee1189472afa4cb14b818761
Mikhail Zaytsev <mikhailzayts@gmail.com>
2022-09-04 21:14:49 +0300

    working version

--- .ccls-cache/@home@mikhail@projects@roadmap@pogoda_espress/main@pogoda_espress.c
+++ .ccls-cache/@home@mikhail@projects@roadmap@pogoda_espress/main@pogoda_espress.c
@@ -1,23 +1,32 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+/** 
+ *  @file       pogoda_espress.c
  *
- * SPDX-License-Identifier: CC0-1.0
+ *  @brief      PoC for Yandex.Pogoda API based ESP32weather station 
+ *
+ *  @author     Mikhail Zaytsev
  */
+
+/********** INCLUDES **********/
+
+#include "sdkconfig.h"
 
 #include <string.h>
 #include <stdio.h>
-#include "sdkconfig.h"
+#include <sys/time.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+
 #include "esp_chip_info.h"
 #include "esp_flash.h"
+#include "esp_timer.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
+
 #include "nvs_flash.h"
-#include "cJSON.h"
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
@@ -25,47 +34,96 @@
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
 
-/* Constants that aren't configurable in menuconfig */
-#define WEB_SERVER "api.weather.yandex.ru"
-#define WEB_PORT "443"
-#define WEB_PATH "/v2/informers?lat=59.9386&lon=30.3141"
+#include "cJSON.h"
+
+/******************** DEFINES ********************/
+
+#define API_YANDEX_HOST "api.weather.yandex.ru"                 /**< Host URL */
+#define API_YANDEX_PORT "80"                                    /**< TLS port */
+#define API_YANDEX_PATH "/v2/informers?lat=59.9386&lon=30.3141" /**< Host path */
+#define API_YANDEX_KEY  "822a9b7c-bfdf-4f43-93b8-ac085bb84c1d"  /**< Yandex API key */
+/**< Yandex API weather GET request */
+#define API_YANDEX_GET_REQ \
+    "GET " API_YANDEX_PATH " HTTP/1.1\r\n" \
+    "Host: " API_YANDEX_HOST "\r\n"  \
+    "X-Yandex-API-Key: " API_YANDEX_KEY "\r\n" \
+    "\r\n"
 
 
-static const char *REQUEST = 
-    "GET " WEB_PATH " HTTP/1.1\r\n"
-    "Host: " WEB_SERVER "\r\n" 
-    "X-Yandex-API-Key: 822a9b7c-bfdf-4f43-93b8-ac085bb84c1d\r\n"
-    "\r\n";
+#define APP_WIFI_SSID          "coreofbear" /**< WiFi SSID */
+#define APP_WIFI_PASS          "12344321"   /**< WiFi password */
+#define APP_WIFI_CONN_TRY_QTY  10           /**< WiFi connection try quantity */
 
+#define WEATHER_GET_TASK_NAME       "Weather get task"  /**< Weather task stack size */
+#define WEATHER_GET_TASK_STACK_SIZE 8192                /**< Weather task stack size */
+#define WEATHER_GET_TASK_PRIORITY   5                   /**< Weather task priority */
+#define WEATHER_GET_RX_BUF_SIZE     1536                /**< Receiving buffer size for weather task */
+#define WEATHER_GET_RX_TIMEOUT_S    10                  /**< Receiving timeout in seconds */
 
-#define ESP_WIFI_SSID      "coreofbear"
-#define ESP_WIFI_PASS      "12344321"
-#define ESP_MAXIMUM_RETRY  10
+#define APP_DELAY_COMMON_MS         5000                /**< Common used delay in milliseconds */
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
+/******************** STRUCTURES, ENUMS, UNIONS ********************/
 
-static int s_retry_num = 0;
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+typedef struct pogoda_ctx_s
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    EventGroupHandle_t wifi_event_group;
+    uint32_t try_num;
+} pogoda_ctx_t;
+
+/******************** GLOBAL VARIABLES ********************/
+
+static pogoda_ctx_t global_ctx = {0};
+
+/******************** PRIVATE FUNCTION PROTOTYPES ********************/
+
+static void net_event_handler(void * ptr_arg, 
+                                esp_event_base_t event_base, 
+                                int32_t event_id, 
+                                void * ptr_event_data);
+static void wifi_init(void);
+static void weather_get_task(void * ptr_params);
+static void weather_display(const char * ptr_str);
+
+/******************** PRIVATE FUNCTIONS ********************/
+
+/**
+ *  @brief      Network operations handler
+ *
+ *  @param[in]  ptr_arg         Argument pointer (don't used)
+ *  @param[in]  event_base      Event base
+ *  @param[in]  event_id        Event ID
+ *  @param[in]  ptr_event_data  Event data pointer (don't used)
+ */  
+static void net_event_handler(void * ptr_arg, 
+                                esp_event_base_t event_base, 
+                                int32_t event_id, 
+                                void * ptr_event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
+    {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < ESP_MAXIMUM_RETRY) {
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) 
+    {
+        if (global_ctx.try_num < APP_WIFI_CONN_TRY_QTY) 
+        {
             esp_wifi_connect();
-            s_retry_num++;
+            global_ctx.try_num++;
         }
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        s_retry_num = 0;
+    } 
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) 
+    {
+        global_ctx.try_num = 0;
     }
 }
 
-void wifi_init_sta(void)
+/**
+ *  @brief WiFi initializaiton and connecting function
+ */
+static void wifi_init(void)
 {
-    s_wifi_event_group = xEventGroupCreate();
+    global_ctx.wifi_event_group = xEventGroupCreate();
+    global_ctx.try_num = 0;
 
     ESP_ERROR_CHECK(esp_netif_init());
 
@@ -79,140 +137,167 @@ void wifi_init_sta(void)
     esp_event_handler_instance_t instance_got_ip;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
-                                                        &event_handler,
+                                                        &net_event_handler,
                                                         NULL,
                                                         &instance_any_id));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
+                                                        &net_event_handler,
                                                         NULL,
                                                         &instance_got_ip));
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = ESP_WIFI_SSID,
-            .password = ESP_WIFI_PASS,
+            .ssid = APP_WIFI_SSID,
+            .password = APP_WIFI_PASS,
             .threshold.authmode = WIFI_AUTH_WPA2_PSK,
             .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
         },
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-static void http_get_task(void *pvParameters)
+/**
+ *  @brief      Weather getting task handler
+ *
+ *  @param[in]  ptr_param   Parameter pointer (don't used)
+ */
+static void weather_get_task(void * ptr_params)
 {
-    printf("Getting weather...\n");
-    const struct addrinfo hints = {
+    const struct addrinfo hints = 
+    {
         .ai_family = AF_INET,
         .ai_socktype = SOCK_STREAM,
     };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[1536];
+    struct addrinfo * ptr_res = NULL;
+    char recv_buf[WEATHER_GET_RX_BUF_SIZE];
 
-    for (uint32_t idx_temp = 0; idx_temp < 1; idx_temp++)
+    int32_t err = getaddrinfo(API_YANDEX_HOST, API_YANDEX_PORT, &hints, &ptr_res);
+    if (0 != err || ptr_res == NULL) 
     {
-        printf("Resolving server address...\n");
-        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
-
-        if(err != 0 || res == NULL) {
-            ESP_LOGE("HTTP", "getaddrinfo(), error %d", err);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Code to print the resolved IP.
-
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            freeaddrinfo(res);
-            ESP_LOGE("HTTP", "socket()");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE("HTTP", "connect()");
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        freeaddrinfo(res);
-
-        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
-            ESP_LOGE("HTTP", "write()");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        printf("Connected to Yandex\n");
-
-        struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-            printf("\n\n%d bytes received\n", r);
-            if (r <= 0)
-            {
-                break;
-            }
-            cJSON * ptr_json_root = cJSON_Parse(strchr(recv_buf, '{'));
-            cJSON * ptr_json_fact = cJSON_GetObjectItem(ptr_json_root, "fact");
-            cJSON * ptr_json_condition = cJSON_GetObjectItem(ptr_json_fact, "condition");
-            cJSON * ptr_json_temp = cJSON_GetObjectItem(ptr_json_fact, "temp");
-            
-            printf("Condition: %s; temperature: %d\n", ptr_json_condition->valuestring, ptr_json_temp->valueint);
-            cJSON_Delete(ptr_json_root);
-            printf("Check\n");
-        } while(r > 0);
-
-        close(s);
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            vTaskDelay(10000 / portTICK_PERIOD_MS);
-        }
+        ESP_LOGE("Get", "getaddrinfo()");
+        return;
     }
+
+    int32_t sock = socket(ptr_res->ai_family, ptr_res->ai_socktype, 0);
+    if (sock < 0) 
+    {
+        freeaddrinfo(ptr_res);
+        ESP_LOGE("Get", "socket()");
+        return;
+    }
+
+    if (connect(sock, ptr_res->ai_addr, ptr_res->ai_addrlen) != 0) 
+    {
+        ESP_LOGE("Get", "connect()");
+        close(sock);
+        return;
+    }
+
+    freeaddrinfo(ptr_res);
+
+    printf("\n%s\n\n", API_YANDEX_GET_REQ);
+
+    if (write(sock, API_YANDEX_GET_REQ, strlen(API_YANDEX_GET_REQ)) < 0) 
+    {
+        ESP_LOGE("Get", "write()");
+        close(sock);
+        return;
+    }
+
+    struct timeval receiving_timeout;
+    receiving_timeout.tv_sec = WEATHER_GET_RX_TIMEOUT_S;
+    receiving_timeout.tv_usec = 0;
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout, sizeof(receiving_timeout)) < 0) 
+    {
+        close(sock);
+        return;
+    }
+
+    int32_t rx_bytes = 0;
+    do {
+        bzero(recv_buf, sizeof(recv_buf));
+        rx_bytes = read(sock, recv_buf, sizeof(recv_buf) - sizeof('\0'));
+
+        for(int32_t idx = 0; idx < rx_bytes; idx++) {
+            putchar(recv_buf[idx]);
+        }
+        putchar('\n');
+
+        if (rx_bytes <= 0)
+        {
+            break;
+        }
+
+        weather_display(strchr(recv_buf, '{'));
+    } while (rx_bytes > 0);
+
+    close(sock);
+    
+    vTaskDelay(APP_DELAY_COMMON_MS / portTICK_PERIOD_MS);
 }
 
+/**
+ *  @brief      Weather parse and display function
+ *
+ *  @param[in]  ptr_str     NULL-terminater string pointer
+ */
+static void weather_display(const char * ptr_str)
+{
+    if (NULL == ptr_str)
+    {
+        ESP_LOGE("Display", "NULL string pointer");
+        return;
+    }
+
+    cJSON * ptr_json_root = cJSON_Parse(ptr_str);
+    cJSON * ptr_json_fact = cJSON_GetObjectItem(ptr_json_root, "fact");
+    cJSON * ptr_json_condition = cJSON_GetObjectItem(ptr_json_fact, "condition");
+    cJSON * ptr_json_temp = cJSON_GetObjectItem(ptr_json_fact, "temp");
+    if ((NULL == ptr_json_condition) ||
+        (NULL == ptr_json_temp))
+    {
+        ESP_LOGE("Display", "Cannot parse weather response");
+        return;
+    }
+    
+    printf("Current weather in Saint-Petersburg:\n");
+    printf("\tCondition: %s\n", ptr_json_condition->valuestring);
+    printf("\tTemperature: %d\n", ptr_json_temp->valueint);
+
+    cJSON_Delete(ptr_json_root);
+}
+
+
+/******************** PUBLIC FUNCTIONS ********************/
+
+/**
+ *  @brief  Application entry point
+ */
 void app_main(void)
 {
-    printf("Connecting to WiFi network...\n");
-    //Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+    {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
-    wifi_init_sta();
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    printf("Connected!\n");
+    wifi_init();
+    vTaskDelay(APP_DELAY_COMMON_MS / portTICK_PERIOD_MS);
 
-    xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+    xTaskCreate(&weather_get_task, 
+                WEATHER_GET_TASK_NAME, 
+                WEATHER_GET_TASK_STACK_SIZE, 
+                NULL, 
+                WEATHER_GET_TASK_PRIORITY, 
+                NULL);
     for (;;)
     {
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(APP_DELAY_COMMON_MS / portTICK_PERIOD_MS);
     }
 }
 
